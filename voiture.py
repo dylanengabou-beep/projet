90bdd26ea2d27aaeefb9de663f086036aa8a4303
Dylan <dylan@example.com>
2026-03-06 11:56:16 -0500

    Creation de la classe Voiture

---
+++ voiture.py
@@ -0,0 +1,18 @@
+class Voiture:
+    def __init__(self, matricule, annee, marque, kilometrage):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur = None
+
+    def afficherInformations(self):
+        print("voiture : ", self.marque)
+        print("matricule : ", self.matricule)
+        print("annee : ", self.annee)
+        print("kilometrage : ", self.kilometrage)
+
+        if self.chauffeur is not None:
+            print("chauffeur : ", self.chauffeur.prenom,  self.chauffeur.nom)
+        else:
+            print("pas de chauffeur assigné")
